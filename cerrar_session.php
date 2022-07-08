3de7289db71e804bddde1c9bfde90331f9c1ebb5
joaquin <joaquinleo002@gmail.com>
2022-07-08 15:37:22 -0300

    cerrar sesion

--- cerrar_session.php
+++ cerrar_session.php
@@ -1,5 +1,11 @@
 <?php
     session_start();
+    error_reporting(0);
+    $varsession = $_SESSION['usuario'];
+    if($varsession == null || $varsession == ""){
+        echo "usted no tiene autorizacion";
+        die();
+    }
     session_destroy();
     header("location:index.php");
 ?>
