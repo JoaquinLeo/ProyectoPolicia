212d47e8f507913a3e9557c2323cb34b7e5c0708
Juliaanbz <julianbaezcarrizo@hotmail.com>
2022-09-30 14:12:44 -0300

    Bootstrap en proceso

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="./estilos/index.css" rel="stylesheet">
+    <link href="./estilos/Logreg.css" rel="stylesheet">
     <title>Document</title>   
     
 </head>
@@ -24,9 +23,9 @@
                     <label for="floatingPassword">Legajo</label>
                 </div>
                 
-                <button class="btn btn-primary btn-lg" type="button" value="Registrar" onclick='registrar()'>Registrar</button>
-                <button class="btn btn-primary btn-lg" type="submit" value="Ingresar" name="ingresar">Ingresar</button>
-            </form>
+                <button class="btn btn-primary btn-lg boton" type="button" value="Registrar" onclick='registrar()'>Registrar</button>
+                <button class="btn btn-primary btn-lg boton" type="submit" value="Ingresar" name="ingresar">Ingresar</button>
+            </form>   
         </main>
         <script>
             function registrar(){
--- registrar.php
+++ registrar.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="./estilos/index.css" rel="stylesheet">
+    <link href="./estilos/Logreg.css" rel="stylesheet">
     <title>Document</title>
 
 </head>
@@ -30,8 +29,8 @@
                 <option value="noadmin">No Admin</option>
                 <option value="admin">Admin</option>
             </select> -->
-            <button class="btn btn-primary btn-lg" type="Sumbit" value="Enviar" name="enviar" >Enviar</button>
-            <button class="btn btn-primary btn-lg" type="button" value="Volver" onclick='volver()'>Volver</button>
+            <button class="btn btn-primary btn-lg boton" type="Sumbit" value="Enviar" name="enviar" >Enviar</button>
+            <button class="btn btn-primary btn-lg boton" type="button" value="Volver" onclick='volver()'>Volver</button>
         </form>
     </main>
     <script>
